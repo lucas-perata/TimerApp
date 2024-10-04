d4d0a1b0b0942914b90bb3aca70d3d5193e272fb
lucas-perata <contacto.perata@gmail.com>
2024-10-04 16:10:06 -0300

    db connection + entities

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using TimerApp.Components;
+using TimerApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,13 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
